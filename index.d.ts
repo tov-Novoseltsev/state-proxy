98da04d394a23b8ec0eae38827bc435757a1948b
Bohdan Zinkevych <bzinkevych@accesssoftek.com>
2022-11-10 12:00:34 +0200

    declare module to named imports

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,40 @@
-declare module 'state-proxy' {
-  interface IValidationResult {
-    isValid: Boolean;
-    validationMessage?: string;
-  }
+export interface IValidationResult {
+  isValid: Boolean;
+  validationMessage?: string;
+}
 
-  interface IGetState {
-    (): { val: any; };
-    getParentState: IGetState;
-  }
+export interface IGetState {
+  (): { val: any; };
+  getParentState: IGetState;
+}
 
-  interface IStateProxyNode {
-    getDefaultState: (overrides?) => IGetState;
-    properties: any;
-    getState: (overrides?) => { val: any; hasChanges: boolean; touched?: boolean; };
-    setTouched: () => void;
-    val: (newVal?: any) => any;
-    ignored: () => boolean;
-    required: () => boolean;
-    validate: (ignoreChanges?: Boolean) => IValidationResult;
-    exposeRequiredErrors: () => void;
-    resetToDefault: () => void;
-    schema: { type: string, required: () => void; maxLength?: number; };
-    setData: (data: any) => any;
-    getData: () => any;
-  }
+export interface IStateProxyNode {
+  getDefaultState: (overrides?) => IGetState;
+  properties: any;
+  getState: (overrides?) => { val: any; hasChanges: boolean; touched?: boolean; };
+  setTouched: () => void;
+  val: (newVal?: any) => any;
+  ignored: () => boolean;
+  required: () => boolean;
+  validate: (ignoreChanges?: Boolean) => IValidationResult;
+  exposeRequiredErrors: () => void;
+  resetToDefault: () => void;
+  schema: { type: string, required: () => void; maxLength?: number; };
+  setData: (data: any) => any;
+  getData: () => any;
+}
 
-  interface ISchemaTypes {
-    dynamic: undefined;
-    bool: 'bool';
-    number: 'number';
-    string: 'string';
-    object: 'object';
-    list: 'list';
-  }
+interface ISchemaTypes {
+  dynamic: undefined;
+  bool: 'bool';
+  number: 'number';
+  string: 'string';
+  object: 'object';
+  list: 'list';
+}
 
-  interface IStateProxy {
-    formValidationResult(validationResult: Boolean, validationMessage: string): IValidationResult;
-    SchemaTypes: ISchemaTypes;
-    create(schema: Object, getState?: () => any, setState?: (newState: any) => void): IStateProxyNode;
-  }
+export interface IStateProxy {
+  formValidationResult(validationResult: Boolean, validationMessage: string): IValidationResult;
+  SchemaTypes: ISchemaTypes;
+  create(schema: Object, getState?: () => any, setState?: (newState: any) => void): IStateProxyNode;
 }
